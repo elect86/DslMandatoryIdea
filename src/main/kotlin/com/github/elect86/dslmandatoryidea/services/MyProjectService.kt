5dc15c8e8238d271324996340f920f745d2f60f8
GitHub Action <action@github.com>
2022-06-17 07:19:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/elect86/dslmandatoryidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/elect86/dslmandatoryidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.elect86.dslmandatoryidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.elect86.dslmandatoryidea.MyBundle
 
 class MyProjectService(project: Project) {
 
